85113a226bfc6da322306d9e851e2c84fce88075
eugeneleejunping <eugeneleejunping@gmail.com>
2025-03-20 01:41:34 +0800

    added pydantic models for cardano blocks from blockfrost

---
+++ src/models/blockfrost_models/cardano_blocks.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+class BlockfrostCardanoBlockInfo(BaseModel):
+    """
+    Represents the block information
+    Source: https://docs.blockfrost.io/#tag/cardano--blocks/GET/blocks/{hash_or_number}
+    """
+    time: int
+    height: int
+    hash: str
+    slot: int
+    epoch: int | None
+    epoch_slot: int | None
+    slot_leader: str
+    size: int
+    tx_count: int
+    output: str | None
+    fees: str | None
+    block_vrf: str | None
+    op_cert: str | None
+    op_cert_counter: str | None
+    previous_block: str | None
+    next_block: str | None
+    confirmations: int
