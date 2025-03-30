07b75a1694602b4b6d06f1ebaf03144ce0a57af5
eugeneleejunping <eugeneleejunping@gmail.com>
2025-03-30 18:07:53 +0800

    update provider_to_s3_import_status_dao.py

--- src/dao/provider_to_s3_import_status_dao.py
+++ src/dao/provider_to_s3_import_status_dao.py
@@ -8,7 +8,6 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncConnection
 from sqlalchemy import (
     Table,
-    insert,
     Insert,
     Select,
     select,
@@ -16,6 +15,7 @@ from sqlalchemy import (
     CursorResult,
     Row,
 )
+from sqlalchemy.dialects.postgresql import insert
 from datetime import datetime
 import logging
 from database_management.cardano.cardano_tables import provider_to_s3_import_status_table
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 setup_logging(logger)
 
 
-class S3ImportStatusDAO:
+class ProviderToS3ImportStatusDAO:
     def __init__(self, connection_string: str) -> None:
         self._engine: AsyncEngine = create_async_engine(connection_string)
         self._table: Table = provider_to_s3_import_status_table
@@ -37,13 +37,14 @@ class S3ImportStatusDAO:
         reraise=True
     )
     async def insert_latest_import_status(
-        self, import_status: ProviderToS3ImportStatusDTO, conn: AsyncConnection
+        self, import_status: ProviderToS3ImportStatusDTO
     ) -> None:
         insert_text_clause: Insert = insert(self._table).values(
             import_status.model_dump()
-        )  # values take in a dict, hence the need to convert the dto to a dict
+        ).on_conflict_do_nothing()  # values take in a dict, hence the need to convert the dto to a dict
         try:
-            await conn.execute(insert_text_clause)
+            async with self._engine.begin() as conn:
+                await conn.execute(insert_text_clause)
         except SQLAlchemyError:
             logger.exception("Failed to insert latest import status")
             raise
@@ -73,20 +74,16 @@ if __name__ == "__main__":
     load_dotenv()
     connection_string: str = os.getenv("ASYNC_PG_CONNECTION_STRING", "")
     print(connection_string)
-    dao: S3ImportStatusDAO = S3ImportStatusDAO(connection_string)
+    dao: ProviderToS3ImportStatusDAO = ProviderToS3ImportStatusDAO(connection_string)
     s3_import_status_dto: ProviderToS3ImportStatusDTO = ProviderToS3ImportStatusDTO(
         table="cardano_blocks",
         block_height=4865265,
         created_at=datetime.utcnow(),
     )
-
-    async def run_insert_status() -> None:
-        engine: AsyncEngine = create_async_engine(connection_string)
-        async with engine.begin() as conn:
-            await dao.insert_latest_import_status(s3_import_status_dto, conn)
-    asyncio.run(run_insert_status())
-
     event_loop: AbstractEventLoop = new_event_loop()
+
+    event_loop.run_until_complete(dao.insert_latest_import_status(s3_import_status_dto))
+
     latest_file_date: datetime | None = event_loop.run_until_complete(
         dao.read_latest_import_status(table="cardano_blocks")
     )
