671d4518bbe40aec9f9ddf3d13f477be06ff96d5
eugeneleejunping <eugeneleejunping@gmail.com>
2025-04-06 00:19:06 +0800

    add cardano block transactions pipeline

--- src/etl_pipelines/cardano_block_transactions_to_s3_pipeline.py
+++ src/etl_pipelines/cardano_block_transactions_to_s3_pipeline.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 from src.extractors.get_block_transactions import CardanoBlockTransactionsExtractor
 from src.models.blockfrost_models.cardano_block_transactions import CardanoBlockTransactions
 from src.dao.provider_to_s3_import_status_dao import ProviderToS3ImportStatusDAO
+from src.dao.s3_to_db_import_status_dao import S3ToDbImportStatusDAO
 from src.file_explorer.s3_file_explorer import S3Explorer
 from src.models.database_transfer_objects.provider_to_s3_import_status import ProviderToS3ImportStatusDTO
 
@@ -26,25 +27,48 @@ class CardanoBlockTransactionsToETLPipeline:
     def __init__(
         self,
         provider_to_s3_import_status_dao: ProviderToS3ImportStatusDAO,
+        s3_to_db_import_status_dao: S3ToDbImportStatusDAO,
         table: str,
         s3_explorer: S3Explorer,
         extractor: CardanoBlockTransactionsExtractor
     ) -> None:
         self._provider_to_s3_import_status_dao: ProviderToS3ImportStatusDAO = provider_to_s3_import_status_dao
+        self._s3_to_db_import_status_dao: S3ToDbImportStatusDAO = s3_to_db_import_status_dao
         self._table: str = table
         self._s3_explorer: S3Explorer = s3_explorer
         self._extractor: CardanoBlockTransactionsExtractor = extractor
 
     async def run(self) -> None:
-        latest_block_height: int | None = (
+        """
+        To check which block to start ingesting from:
+        - compare last block_height of cardano_blocks column of s3_to_db_import_status_table VS last block_height of cardano_block_transactions provider_to_s3_import_status_table from database
+        if cardano_blocks_tx_last_height >= cardano_blocks_last_height: do nothing
+        else:
+            start_block = provider_to_s3_import_status_table.read_latest_import_status("cardano_block_transactions) + 1
+            end_block = provider_to_s3_import_status_table.read_latest_import_status("cardano_blocks)
+        """
+        blocks_tx_latest_block_height: int | None = (
             await self._provider_to_s3_import_status_dao.read_latest_import_status(
-                self._table
+                "cardano_block_transactions"
             )
         )
-        num_of_blocks: int = 500
-        start_block_height: int = (latest_block_height + 1) or 11292700
+        print(f"blocks_tx_latest_block_height = {blocks_tx_latest_block_height}")
+        blocks_latest_block_height: int | None = (
+            await self._s3_to_db_import_status_dao.read_latest_import_status(
+                "cardano_blocks"
+            )
+        )
+        print(f"blocks_latest_block_height = {blocks_latest_block_height}")
+        if blocks_tx_latest_block_height >= blocks_latest_block_height:
+            print(f"block transactions in S3 up to date")
+            return None
+
+        # num_of_blocks: int = 500
+        # start_block_height: int = (latest_block_height + 1) or 11292700
+
+        start_block_height: int = blocks_tx_latest_block_height or 1
         print(f"start_block_height = {start_block_height}")
-        end_block_height: int = start_block_height + num_of_blocks - 1
+        end_block_height: int = blocks_latest_block_height
         print(f"end block height = {end_block_height}")
         curr_block_height: int = start_block_height
         # list to collect all block transactions data into a list of dict
@@ -85,9 +109,13 @@ def run():
     provider_to_s3_import_status_dao: ProviderToS3ImportStatusDAO = ProviderToS3ImportStatusDAO(
         os.getenv("ASYNC_PG_CONNECTION_STRING", "")
     )
+    s3_to_db_import_status_dao: S3ToDbImportStatusDAO = S3ToDbImportStatusDAO(
+        os.getenv("ASYNC_PG_CONNECTION_STRING", "")
+    )
     extractor: CardanoBlockTransactionsExtractor = CardanoBlockTransactionsExtractor()
     cardano_block_tx_to_s3_etl_pipeline: CardanoBlockTransactionsToETLPipeline = CardanoBlockTransactionsToETLPipeline(
         provider_to_s3_import_status_dao=provider_to_s3_import_status_dao,
+        s3_to_db_import_status_dao=s3_to_db_import_status_dao,
         table="cardano_block_transactions",
         s3_explorer=s3_explorer,
         extractor=extractor
