3142482307b6571551b06a6b9a2e94b9cbdfaa88
eugeneleejunping <eugeneleejunping@gmail.com>
2025-04-07 22:57:45 +0800

    changed block_height column to file_modified_date and removed static method to create_import_status

--- src/models/database_transfer_objects/s3_to_db_import_status_dto.py
+++ src/models/database_transfer_objects/s3_to_db_import_status_dto.py
@@ -4,14 +4,14 @@ from pydantic import BaseModel
 
 class S3ToDBImportStatusDTO(BaseModel):
     table: str
-    block_height: int
+    file_modified_date: datetime
     created_at: datetime
 
-    @staticmethod
-    def create_import_status(table: str, block_height: int) -> "S3ToDBImportStatusDTO":
-        return S3ToDBImportStatusDTO(
-            table=table,
-            block_height=block_height,
-            created_at=datetime.utcnow(),
-        )
+    # @staticmethod
+    # def create_import_status(table: str, block_height: int) -> "S3ToDBImportStatusDTO":
+    #     return S3ToDBImportStatusDTO(
+    #         table=table,
+    #         file_modified_date=,
+    #         created_at=datetime.utcnow(),
+    #     )
 
