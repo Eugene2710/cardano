feb2b2780d3758a4bb3fb6a18c7380a463e121d5
eugeneleejunping <eugeneleejunping@gmail.com>
2025-05-02 17:28:15 +0800

    refactored get_transactions_from_s3 to synchronous extraction rather than async since there is nothing async about it

--- src/extractors/get_transactions_from_s3.py
+++ src/extractors/get_transactions_from_s3.py
@@ -1,12 +1,11 @@
 import io
-import asyncio
 import json
 import os
 from typing import Any
 from dotenv import load_dotenv
 from pprint import pprint
+from retry import retry
 
-from tenacity import retry, wait_fixed, stop_after_attempt
 from src.models.blockfrost_models.raw_cardano_transactions import CardanoTransactions
 from src.models.database_transfer_objects.cardano_transactions import CardanoTransactionsDTO
 from src.models.database_transfer_objects.cardano_transactions_output_amount import CardanoTransactionsOutputAmountDTO
@@ -18,11 +17,13 @@ class CardanoTransactionsS3Extractor:
         self._s3_explorer: S3Explorer = s3_explorer
 
     @retry(
-        wait=wait_fixed(0.01),
-        stop=stop_after_attempt(5),
-        reraise=True,
+        tries=5,
+        delay=0.1,
+        max_delay=0.3375,
+        backoff=1.5,
+        jitter=(-0.01, 0.01),
     )
-    async def get_tx_from_s3(self, s3_path: str) -> list[CardanoTransactionsDTO]:
+    def get_tx_from_s3(self, s3_path: str) -> list[CardanoTransactionsDTO]:
         """
         - get specified json file from s3 using S3Explorer.download_to_buffer
         - add json file to database
@@ -41,11 +42,13 @@ class CardanoTransactionsS3Extractor:
         return dtos
 
     @retry(
-        wait=wait_fixed(0.01),
-        stop=stop_after_attempt(5),
-        reraise=True,
+        tries=5,
+        delay=0.1,
+        max_delay=0.3375,
+        backoff=1.5,
+        jitter=(-0.01, 0.01),
     )
-    async def get_tx_output_amount_from_s3(self, s3_path: str) -> list[CardanoTransactionsOutputAmountDTO]:
+    def get_tx_output_amount_from_s3(self, s3_path: str) -> list[CardanoTransactionsOutputAmountDTO]:
         """
         - get specified json file from s3 using S3Explorer.download_to_buffer
         - add json file to database
@@ -73,11 +76,8 @@ if __name__ == "__main__":
         secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY", ""),
     )
     extractor: CardanoTransactionsS3Extractor = CardanoTransactionsS3Extractor(s3_explorer=s3_explorer)
-    event_loop: asyncio.AbstractEventLoop = asyncio.new_event_loop()
-    tx_res: list[CardanoTransactionsDTO] = event_loop.run_until_complete(
-        extractor.get_tx_from_s3(s3_path="cardano/transactions/raw/11292900/cardano_transactions_11292900.json")
-    )
-    tx_output_amt_res: list[CardanoTransactionsOutputAmountDTO] = event_loop.run_until_complete(
-        extractor.get_tx_output_amount_from_s3(s3_path="cardano/transactions/raw/11292900/cardano_transactions_11292900.json")
-    )
-    pprint(tx_output_amt_res)
+    tx_res: list[CardanoTransactionsDTO] = extractor.get_tx_from_s3(s3_path="cardano/transactions/raw/11292900/cardano_transactions_11292900.json")
+    # tx_output_amt_res: list[CardanoTransactionsOutputAmountDTO] = event_loop.run_until_complete(
+    #     extractor.get_tx_output_amount_from_s3(s3_path="cardano/transactions/raw/11292900/cardano_transactions_11292900.json")
+    # )
+    pprint(tx_res)
