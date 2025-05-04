3e999e4a4e38ce7a394dd558a16c107fb1e7fa36
eugeneleejunping <eugeneleejunping@gmail.com>
2025-05-05 02:30:57 +0800

    add dto from cardano transaction utxo

---
+++ src/models/database_transfer_objects/cardano_transactions_utxo_dto.py
@@ -0,0 +1,118 @@
+import uuid
+
+from pydantic import BaseModel
+from datetime import datetime
+from src.models.blockfrost_models.cardano_transaction_utxo import TransactionUTxO
+
+
+class TxAmountDTO(BaseModel):
+    id: uuid.UUID
+    tx_utxo_hash: str
+    unit: str
+    quantity: str
+
+
+class CardanoTxUtxoInputDTO(BaseModel):
+    id: uuid.UUID
+    hash: str  # transaction hash (same as `hash` in parent)
+    address: str
+    tx_utxo_hash: str
+    output_index: int
+    data_hash: str | None = None
+    inline_datum: str | None = None
+    reference_script_hash: str | None = None
+    collateral: bool
+    reference: bool | None = None
+    created_at: datetime
+    amounts: list[TxAmountDTO]
+
+
+class CardanoTxUtxoOutputDTO(BaseModel):
+    id: uuid.UUID
+    hash: str  # transaction hash (same as `hash` in parent)
+    address: str
+    output_index: int
+    data_hash: str | None = None
+    inline_datum: str | None = None
+    collateral: bool
+    reference_script_hash: str | None = None
+    consumed_by_tx: str | None = None
+    created_at: datetime
+    amounts: list[TxAmountDTO]
+
+
+class CardanoTransactionUtxoDTO(BaseModel):
+    """
+    - convert time from unix to
+    - include a created_at column of type datetime to specify the time cardano transaction utxo was ingested
+    """
+    hash: str # tx_hash
+    # block_height: int
+    created_at: datetime
+    inputs: list[CardanoTxUtxoInputDTO]
+    outputs: list[CardanoTxUtxoOutputDTO]
+
+    @staticmethod
+    def from_raw_cardano_tx_utxo(hash: str, input: TransactionUTxO) -> "CardanoTransactionUtxoDTO":
+        parent_hash = hash
+        input_dtos: list[CardanoTxUtxoInputDTO] = []
+        for inp in input.inputs:
+            amounts = [
+                TxAmountDTO(
+                    id=uuid.uuid4(),
+                    tx_utxo_hash=inp.tx_hash,
+                    unit=a.unit,
+                    quantity=a.quantity,
+                ) for a in inp.amount
+            ]
+            input_dtos.append(
+                CardanoTxUtxoInputDTO(
+                    id=uuid.uuid4(),
+                    hash=parent_hash,
+                    address=inp.address,
+                    tx_utxo_hash=inp.tx_hash,
+                    output_index=inp.output_index,
+                    data_hash=inp.data_hash,
+                    inline_datum=inp.inline_datum,
+                    reference_script_hash=inp.reference_script_hash,
+                    collateral=inp.collateral,
+                    # reference=inp.reference if inp.reference else None,
+                    amounts=amounts,
+                    created_at=datetime.utcnow()
+                )
+            )
+
+        output_dtos: list[CardanoTxUtxoOutputDTO] = []
+        for out in input.outputs:
+            amounts = [
+                TxAmountDTO(
+                    id=uuid.uuid4(),
+                    tx_utxo_hash=parent_hash,
+                    unit=a.unit,
+                    quantity=a.quantity,
+                ) for a in out.amount
+            ]
+            output_dtos.append(
+                CardanoTxUtxoOutputDTO(
+                    id=uuid.uuid4(),
+                    hash=parent_hash,
+                    address=out.address,
+                    output_index=out.output_index,
+                    data_hash=out.data_hash,
+                    inline_datum=out.inline_datum,
+                    collateral=out.collateral,
+                    reference_script_hash=out.reference_script_hash,
+                    consumed_by_tx=out.consumed_by_tx,
+                    amounts=amounts,
+                    created_at=datetime.utcnow()
+                )
+            )
+
+        return CardanoTransactionUtxoDTO(
+            hash=input.hash,
+            created_at=datetime.utcnow(),
+            inputs=input_dtos,
+            outputs=output_dtos,
+        )
+
+
