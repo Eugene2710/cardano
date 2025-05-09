debef2ca2a5517dccd763cfb13ac4651433b66c8
eugeneleejunping <eugeneleejunping@gmail.com>
2025-05-10 01:41:51 +0800

    changed alterate start_block_height to 11292700 instead of 1

--- src/etl_pipelines/cardano_block_transactions_to_s3_pipeline.py
+++ src/etl_pipelines/cardano_block_transactions_to_s3_pipeline.py
@@ -64,7 +64,7 @@ class CardanoBlockTransactionsToETLPipeline:
             print(f"block transactions in S3 up to date")
             return None
 
-        start_block_height: int = blocks_tx_latest_block_height+1 or 1
+        start_block_height: int = blocks_tx_latest_block_height+1 or 11292700
         print(f"start_block_height = {start_block_height}")
         end_block_height: int = blocks_latest_block_height
         print(f"end block height = {end_block_height}")
